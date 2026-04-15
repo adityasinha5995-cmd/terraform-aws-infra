431774c3d7e10573b4156dfa01298e52fb7b2e76
Aditya Sinha <adityasinha5995@gmail.com>
2026-04-16 00:59:16 +0530

    fix: terraform formatting

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,6 +1,6 @@
-output "alb_dns_name"  { value = module.alb.alb_dns_name }
-output "bucket_id"     { value = module.s3.bucket_id }
-output "db_endpoint"   { value = module.rds.db_endpoint }
+output "alb_dns_name" { value = module.alb.alb_dns_name }
+output "bucket_id" { value = module.s3.bucket_id }
+output "db_endpoint" { value = module.rds.db_endpoint }
 output "nat_public_ip" { value = module.nat.nat_public_ip }
-output "asg_name"      { value = module.asg.asg_name }
-output "dashboard"     { value = module.cloudwatch.dashboard_name }
+output "asg_name" { value = module.asg.asg_name }
+output "dashboard" { value = module.cloudwatch.dashboard_name }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,27 +1,27 @@
-variable "aws_region"   { default = "us-east-1" }
+variable "aws_region" { default = "us-east-1" }
 variable "project_name" { default = "myapp" }
-variable "environment"  { default = "dev" }
-variable "vpc_cidr"     { default = "10.0.0.0/16" }
+variable "environment" { default = "dev" }
+variable "vpc_cidr" { default = "10.0.0.0/16" }
 
 variable "instance_type" { default = "t3.micro" }
 
 # ASG settings
-variable "asg_min_size"        { default = 1 }
-variable "asg_max_size"        { default = 4 }
+variable "asg_min_size" { default = 1 }
+variable "asg_max_size" { default = 4 }
 variable "asg_desired_capacity" { default = 2 }
 
 # RDS settings
-variable "db_name"     { default = "appdb" }
+variable "db_name" { default = "appdb" }
 variable "db_username" { default = "admin" }
 variable "db_password" {
   type      = string
   sensitive = true
 }
-variable "db_instance_class"   { default = "db.t3.micro" }
-variable "db_storage"          { default = 20 }
+variable "db_instance_class" { default = "db.t3.micro" }
+variable "db_storage" { default = 20 }
 variable "deletion_protection" { default = false }
 variable "skip_final_snapshot" { default = true }
-variable "backup_retention"    { default = 1 }
+variable "backup_retention" { default = 1 }
 
 # CloudWatch
 variable "alert_email" {
