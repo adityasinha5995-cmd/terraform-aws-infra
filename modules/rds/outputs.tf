fd337473dfe0996dfdc78e5830972db0cea33573
Aditya Sinha <adityasinha5995@gmail.com>
2026-04-17 01:50:23 +0530

    Fix RDS outputs corruption

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,14 +1,11 @@
 output "db_endpoint" {
-  description = "The connection endpoint for the database"
-  value       = aws_db_instance.main.endpoint
+  value = aws_db_instance.main.endpoint
 }
 
 output "db_name" {
-  description = "The name of the database"
-  value       = aws_db_instance.main.db_name
+  value = aws_db_instance.main.db_name
 }
 
 output "db_port" {
-  description = "The port the database listens on"
-  value       = aws_db_instance.main.port
+  value = aws_db_instance.main.port
 }
